9c752d32d5cd5bc145d4191abc74aa78df69fba4
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-02-03 20:19:59 -0500

    setup.py, .gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ckanexport',
+    version='0.1-alpha',
+    description=
+        'A tool for exporting data from CKAN in a friendly format',
+    license='MIT',
+    author='U.S. Open Data Institute',
+    author_email='contact@boxkite.ca',
+    url='https://github.com/opendata/CKAN-Export',
+    packages=[
+        'ckanexport',
+        ],
+    test_suite='',
+    zip_safe=False,
+    entry_points = """
+        [console_scripts]
+        ckanexport=ckanexport.cli.main:main
+        """
+    )
